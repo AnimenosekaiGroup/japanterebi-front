521c66e4422bf7471ce8edb23d7d350ad2d1a310
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2021-04-20 19:54:08 +0200

    [update] Using the PostCSS plugins

--- postcss.config.js
+++ postcss.config.js
@@ -6,8 +6,8 @@
 
 module.exports = {
   plugins: [
-    require('postcss-preset-env'),
-    require('autoprefixer'),
-    require('cssnano-preset-default')
+    require('postcss-preset-env')({}),
+    require('autoprefixer')({}),
+    require('cssnano')({preset: 'default'})
   ]
 }
